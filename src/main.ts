4d54c4422428e3b8196a94073abd0765203bea1a
jwvonk <jwavonk@gmail.com>
2023-10-18 11:32:14 -0700

    Step 4 Done

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ const ctx = canvas.getContext("2d")!;
 
 const lines: { x: number; y: number }[][] = [];
 
+const redoLines: { x: number; y: number }[][] = [];
+
 let currentLine: { x: number; y: number }[] = [];
 
 const cursor = { active: false, x: 0, y: 0 };
@@ -30,7 +32,9 @@ canvas.addEventListener("mousedown", (e) => {
   cursor.y = e.offsetY;
   currentLine = [];
   lines.push(currentLine);
+  redoLines.splice(0, redoLines.length);
   currentLine.push({ x: cursor.x, y: cursor.y });
+
   canvas.dispatchEvent(event);
 });
 
@@ -63,12 +67,36 @@ canvas.addEventListener("drawing-changed", () => {
   }
 });
 
+app.append(document.createElement("br"));
+app.append(document.createElement("br"));
+
 const clearButton = document.createElement("button");
 clearButton.innerHTML = "clear";
-app.append(document.createElement("br"));
 app.append(clearButton);
 
 clearButton.addEventListener("click", () => {
   lines.splice(0, lines.length);
   canvas.dispatchEvent(event);
+});
+
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "undo";
+app.append(undoButton);
+
+undoButton.addEventListener("click", () => {
+  if (lines.length > 0) {
+    redoLines.push(lines.pop()!);
+    canvas.dispatchEvent(event);
+  }
+});
+
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "redo";
+app.append(redoButton);
+
+redoButton.addEventListener("click", () => {
+  if (redoLines.length > 0) {
+    lines.push(redoLines.pop()!);
+    canvas.dispatchEvent(event);
+  }
 });
