b3c55a69f5a9da82848b253e2735479ea4c11e3d
jwvonk <jwavonk@gmail.com>
2023-10-18 11:31:17 -0700

    Step 1 Done

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My App";
+const appTitle = "Sticker Sketchbook";
 
-document.title = gameName;
+document.title = appTitle;
 
 const header = document.createElement("h1");
-header.innerHTML = gameName;
+header.innerHTML = appTitle;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 256;
+app.append(canvas);
